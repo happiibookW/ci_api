7466a2406e490f49643fb2ccac6daf37a7f0d1ba
Dev Geektech <dev@geekinformatic.com>
2024-04-09 15:45:41 +0530

    Resolve email issue

--- application/controllers/API/v1/common/EmailVerification.php
+++ application/controllers/API/v1/common/EmailVerification.php
@@ -12,6 +12,7 @@ class EmailVerification extends REST_Controller
 
         parent::__construct();
         $this->load->model('UserModel');
+		$this->load->library('email');
     }
 
     public function index_post()
@@ -30,7 +31,41 @@ class EmailVerification extends REST_Controller
 				if($user==false && $business==false){
                 if ($this->UserModel->addEmail($emailData, "mstauthorization") == true) {
                     $message="Your verification code is: ".$verificationCode;
-                    $sendMail = sendMail($email,"Verfication Code By Happiverse",$message);
+
+
+					$config['protocol'] = 'smtp';
+					$config['smtp_host'] = 'smtp.gmail.com'; // Your SMTP host
+					$config['smtp_user'] = 'abhimanyu.geektech@gmail.com'; // Your email address
+					$config['smtp_pass'] = 'mtkloecutdwlkzgz'; // Your email password
+					$config['smtp_port'] = 587; // SMTP port (Gmail uses 587)
+					$config['smtp_crypto'] = 'tls'; // Encryption type
+					$config['charset'] = 'utf-8';
+					$config['mailtype'] = 'html';
+					$config['newline'] = "\r\n";
+			
+					$this->email->initialize($config);
+			
+					// Set email parameters
+					$this->email->from('abhimanyu.geektech@gmail.com', 'Your Name');
+					$this->email->to($email);
+					$this->email->subject('Verfication Code By Happiverse');
+					$this->email->message($message);
+			
+					// Send email
+					if ($this->email->send()) {
+						$this->response(array(
+							"status" => DATA_SAVE,
+							"message" => DATA_SAVE_MESSAGE,
+							"verificationCode"=>$verificationCode,
+						// "userId"=>$responseData['userId'],
+						), REST_Controller::HTTP_OK);
+					} else {
+						$this->response(array(
+							"status" => DATA_SAVE_ERROR,
+							"message" => DATA_SAVE_MESSAGE
+						), REST_Controller::HTTP_OK);
+					}
+                    // $sendMail = sendMail($email,"Verfication Code By Happiverse",$message);
 					// if($sendMail){
 					// 	// $responseData=$this->UserModel->getUserId($email);
 					// 	$this->response(array(
@@ -45,12 +80,7 @@ class EmailVerification extends REST_Controller
 					// 		"message" => DATA_SAVE_ERROR_MESSAGE
 					// 	), REST_Controller::HTTP_OK);
 					// }
-					$this->response(array(
-						"status" => DATA_SAVE,
-						"message" => DATA_SAVE_MESSAGE,
-						"verificationCode"=>$verificationCode,
-					// "userId"=>$responseData['userId'],
-					), REST_Controller::HTTP_OK);
+				
                 } else {
                     $this->response(array(
                         "status" => DATA_SAVE_ERROR,
