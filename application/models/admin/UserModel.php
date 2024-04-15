38eca00721ff61e2fd06c8e71070805415f1c921
Dev Geektech <dev@geekinformatic.com>
2024-04-15 11:33:30 +0530

    Add siteurl to imageurl

--- application/models/admin/UserModel.php
+++ application/models/admin/UserModel.php
@@ -130,7 +130,7 @@ class UserModel extends CI_Model
             $finalData['email'] = $users['email'];
             $finalData['DOB'] = $users['DOB'];
             $finalData['martialStatus'] = $users['martialStatus'];
-            $finalData['profileImageUrl'] = $users['profileImageUrl'];
+            $finalData['profileImageUrl'] = 'http://18.117.21.112/hapiverse/'.$users['profileImageUrl'];
             $finalData['gender'] = $users['gender'];
             $finalData['city'] = $users['city'];
             $finalData['postCode'] = $users['postCode'];
