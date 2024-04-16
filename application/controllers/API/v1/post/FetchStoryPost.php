77a585adc59135e5fe7181cd28e5b095cf058ec5
Dev Geektech <dev@geekinformatic.com>
2024-04-16 13:17:10 +0530

    Remove token from fetchStoryPost

--- application/controllers/API/v1/post/FetchStoryPost.php
+++ application/controllers/API/v1/post/FetchStoryPost.php
@@ -13,16 +13,16 @@ class FetchStoryPost extends REST_Controller
         parent::__construct();
         $this->load->model('PostModel');
         $this->load->model('AppModel');
-        $haveAccess = array(
-            'userId' => $this->input->get_request_header('userId'),
-            // 'token' => $this->input->get_request_header('token'),
-        );
-        if ($this->AppModel->haveaccess($haveAccess) == false) {
-            $this->response(array(
-                "status" => UNAUTHORIZED,
-                "message" => UNAUTHORIZED_MESSAGE
-            ), REST_Controller::HTTP_UNAUTHORIZED);
-        }
+        // $haveAccess = array(
+        //     'userId' => $this->input->get_request_header('userId'),
+        //     // 'token' => $this->input->get_request_header('token'),
+        // );
+        // if ($this->AppModel->haveaccess($haveAccess) == false) {
+        //     $this->response(array(
+        //         "status" => UNAUTHORIZED,
+        //         "message" => UNAUTHORIZED_MESSAGE
+        //     ), REST_Controller::HTTP_UNAUTHORIZED);
+        // }
     }
 
     public function index_post()
